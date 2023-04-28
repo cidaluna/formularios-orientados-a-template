7f504ac556b48bb5512fd83509a393053eae777a
cidaluna <cidalunna@hotmail.com>
2023-04-27 22:02:03 -0300

    Update components and visualizations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cadastro',
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+  {
+    path: 'sucesso',
+    component: SucessoCadastroComponent
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
